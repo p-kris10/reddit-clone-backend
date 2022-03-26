fff9415ad98f149133a7d40a82fd77413ef99933
Deadmau26 <26pkristen@gmail.com>
2022-03-26 18:48:11 +0530

    added sleep util

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
+import { COOKIE_NAME, __prod__ } from "./constants";
 import "reflect-metadata";
 import { Post } from "./entities/Post";
 import express from 'express';
@@ -33,7 +33,7 @@ const main = async() =>{
 
     app.use(
         session({
-        name : "qid",
+        name : COOKIE_NAME,
         store: new RedisStore({ client: redisClient, 
             disableTouch: true}),
         cookie : {
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -3,6 +3,7 @@ import { MyContext } from "src/types";
 import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql"; 
 import argon2 from "argon2";
 import {EntityManager} from "@mikro-orm/postgresql"
+import { COOKIE_NAME } from '../constants';
 @InputType()
 class UsernamePasswordInput{
     @Field()
@@ -130,5 +131,23 @@ export class UserResolver{
         //store user id session cookie and keep them logged in
         req.session.userId = user.id;
         return {user};
+    }
+
+    @Mutation(()=> Boolean)
+    logout(
+        @Ctx() {req,res} : MyContext
+    ){
+        
+        return new Promise(resolve => req.session.destroy(err => {
+            res.clearCookie(COOKIE_NAME)
+            if(err)
+            {
+                console.log(err)
+                resolve(false)
+                return
+            }
+            resolve(true)
+        })
+        );
     }
 }
